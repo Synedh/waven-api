afc12683cf4cc47d32677184cda65ac9afb09513
Synedh <contact@synedh.fr>
2019-02-27 18:09:58 +0100

    Removed unused web_tools.

--- web_tools/controllers/uploadToolController.js
+++
@@ -1,3 +0,0 @@
-exports.page = function(req, res) {
-    res.send('Test');
-}
--- web_tools/routes/wavenToolsRoutes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-    const uploadTool = require('../controllers/uploadToolController');
-
-    app.route('/uploadImage')
-        .get(uploadTool.page);
-}
